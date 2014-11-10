39c602be671af35977aa58ea6100c3aaacde8a02
John Mulholland <jmulholland@John-iMac-2.local>
2014-11-10 15:09:09 +0000

    Before arrays

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "BengalTiger"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        println("My tiger's name is \(myTiger.name) its age is \(myTiger.age) and the breed is \(myTiger.breed) and its picture is \(myTiger.image)")
+        
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        myImageView.image = myTiger.image
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.age = 2
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.age = 4
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.age = 5
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItem(sender: UIBarButtonItem) {
+    }
 
 }
 
